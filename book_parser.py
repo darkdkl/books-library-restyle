a4e8d57c3e2d9e35b5552e1b6e9656dd2964cc18
Dark_Dmake <dark.dmake@gmail.com>
2020-02-28 23:12:28 +0300

    download image function

--- book_parser.py
+++ book_parser.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urljoin
 
 
 def get_book_info(book_id):
-    response = requests.get(f'http://tululu.org/b{book_id}/',
-                            allow_redirects=False)
+    host_url = "http://tululu.org"
+    response = requests.get(f'{host_url}/b{book_id}/', allow_redirects=False)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    book_info = soup.find('div', id="content").find('h1').text
+    book_content = soup.find('div', id="content")
+    book_image_path = book_content.find(
+                            'div', class_="bookimage").find('img').get('src')
+    book_image_name = book_image_path.rsplit('/')[-1]
+    book_info = book_content.find('h1').text
     book_name, author = book_info.replace('\xa0', "").split('::')
-    return f"{book_name.strip()} ({author.strip()}).txt"
+    book_title=f"{book_name.strip()} ({author.strip()}).txt"
+    return book_title, urljoin(host_url,book_image_path),book_image_name
 
 
 if __name__ == "__main__":
-    print(get_book_info(1) )
+    print(get_book_info(9))
--- main.py
+++ main.py
@@ -3,25 +3,42 @@ import os
 import time
 from book_parser import get_book_info
 from pathvalidate import sanitize_filename
+from urllib.parse import urlparse
+
+def get_norm_file_path(folder,filename):
+    return os.path.join(folder, sanitize_filename(filename))
 
 def download_txt(url, filename, folder='books/'):
 
     os.makedirs(folder, exist_ok=True)
     response = requests.get(url)
     if response.ok:
-        file_path = os.path.join(folder, sanitize_filename(filename))
+        file_path = get_norm_file_path(folder,filename)
         with open(file_path, 'w') as file_data:
             file_data.write(response.text)
         return file_path
 
+def download_image(url, filename,folder='books/img'):
+    os.makedirs(folder, exist_ok=True)
+    response=requests.get(url)
+    if response.ok:
+        file_path = get_norm_file_path(folder,filename)
+        with open (file_path,'wb') as img_file:
+            img_file.write(response.content)
+
+
+    
 
 def main():
 
     for num in range(11):
         url = f"http://tululu.org/txt.php?id={num}"
+
         response = requests.get(url, allow_redirects=False)
         if response.status_code == 200:
-            print(download_txt(url, get_book_info(num)))
+            txt_name, img_url,book_image_name =  get_book_info(num)
+            download_image(img_url,book_image_name)
+            # print(download_txt(url, get_book_info(txt_name)))
             time.sleep(1)
 
 
